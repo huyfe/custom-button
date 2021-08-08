10cdbcf55b748a857e058d5f03a5e68b2b507e8c
Huy FE <huyyntran@gmail.com>
2021-08-09 00:36:59 +0700

    refactor code

--- app.js
+++ app.js
@@ -1,111 +1,100 @@
-function main() {
-    parseCss();
-}
-main();
 
-
-function parseCss() {
-    let codeString = `.button {
-    text-align: center;
-    text-decoration: none;
-    `;
-    let endCodeString = "}";
-    let inputCode = document.getElementById("inputCode");
-
+(function parseCss() {
+    let outputCode = document.getElementById("outputCode");
     let button = document.getElementById("button");
-
-    // Input css
+    // Element input
     let color = document.getElementById("color");
     let bgColor = document.getElementById("bgColor");
     let paddingX = document.getElementById("paddingX");
     let paddingY = document.getElementById("paddingY");
+    let border = document.getElementById("border");
+    let borderColor = document.getElementById("borderColor");
     let borderRadius = document.getElementById("borderRadius");
+    // Default array
+    let arrInput = [
+        {
+            styleCss: "color",
+            value: `${color.value}`
+        },
+        {
+            styleCss: "background-color",
+            value: `${bgColor.value}`
+        },
+        {
+            styleCss: "padding",
+            value: `${paddingX.value}px ${paddingY.value}px`
+        },
+        {
+            styleCss: "border",
+            value: `${border.value}px solid ${borderColor.value}`
+        },
+        {
+            styleCss: "border-radius",
+            value: `${borderRadius.value}px`
+        }
+    ];
 
-    let codeColor = "";
-    let codeBgColor = "";
-    let codePaddingX = "";
-    let codePaddingY = "";
-    let codeBorderRadius = "";
-    let result = "";
+    let code = "";
+    arrInput.forEach((item) => {
+        if (item.styleCss == "color") button.style.color = item.value;
+        if (item.styleCss == "background-color") button.style.backgroundColor = item.value;
+        if (item.styleCss == "padding") button.style.padding = item.value;
+        if (item.styleCss == "border") button.style.border = item.value;
+        if (item.styleCss == "border-radius") button.style.borderRadius = item.value;
+        code += `\n\t${item.styleCss}: ${item.value};`;
+    });
+    let result = `.button { \n\ttext-align: center; \n\ttext-decoration: none;${code}\n\tcursor: pointer;\n}`;
+    outputCode.innerHTML = result;
 
-    result = `${codeString}${endCodeString}`;
-    inputCode.innerHTML = result;
+    // Gọi hàm để add event cho mỗi element khi input change
+    changeValueInput(color);
+    changeValueInput(bgColor);
+    changeValueInput(paddingX);
+    changeValueInput(paddingY);
+    changeValueInput(border);
+    changeValueInput(borderColor);
+    changeValueInput(borderRadius);
 
-    color.addEventListener("input", function() {
-        codeColor = `color: ${color.value};`;
-        result = `${codeString}${codeColor}
-    ${codeBgColor}
-    ${codePaddingX}
-    ${codeBorderRadius}
-${endCodeString}`;
-
-        inputCode.innerHTML = result;
-
-        button.style.color = color.value;
+    function changeValueInput(inputElem) {
+        inputElem.addEventListener("input", function () {
+            // Set to arrInput variable current value of all input
+            arrInput = [
+                {
+                    styleCss: "color",
+                    value: `${color.value}`
+                },
+                {
+                    styleCss: "background-color",
+                    value: `${bgColor.value}`
+                },
+                {
+                    styleCss: "padding",
+                    value: `${paddingX.value}px ${paddingY.value}px`
+                },
+                {
+                    styleCss: "border",
+                    value: `${border.value}px solid ${borderColor.value}`
+                },
+                {
+                    styleCss: "border-radius",
+                    value: `${borderRadius.value}px`
+                }
+            ];
+            code = "";
+            arrInput.forEach((item) => {
+                if (item.styleCss == "color") button.style.color = item.value;
+                if (item.styleCss == "background-color") button.style.backgroundColor = item.value;
+                if (item.styleCss == "padding") button.style.padding = item.value;
+                if (item.styleCss == "border") button.style.border = item.value;
+                if (item.styleCss == "border-radius") button.style.borderRadius = item.value;
+                code += `\n\t${item.styleCss}: ${item.value};`;
+            });
+            result = `.button { \n\ttext-align: center; \n\ttext-decoration: none;${code}\n\tcursor: pointer;\n}`;
+            outputCode.innerHTML = result;
+        })
+    }
+    // Event click to copy code
+    document.querySelector("#copy").addEventListener("click", function () {
+        window.prompt("Copy to clipboard: Ctrl+C, Enter", outputCode.innerHTML);
     })
-
-    bgColor.addEventListener("input", function() {
-        codeBgColor = `background-color: ${bgColor.value};`;
-        result = `${codeString}${codeColor}
-    ${codeBgColor}
-    ${codePaddingX}
-    ${codeBorderRadius}
-${endCodeString}`;
-
-        inputCode.innerHTML = result;
-
-        button.style.backgroundColor = bgColor.value;
-    })
-
-    paddingX.addEventListener("input", function() {
-        codePaddingX = `padding: ${paddingX.value}px ${paddingY.value}px;`;
-        result = `${codeString}${codeColor}
-    ${codeBgColor}
-    ${codePaddingX}
-    ${codeBorderRadius}
-${endCodeString}`;
-
-        inputCode.innerHTML = result;
-
-        button.style.padding = paddingX.value + "px " + paddingY.value + "px";
-
-    })
-
-    paddingY.addEventListener("input", function() {
-        codePaddingX = `padding: ${paddingX.value}px ${paddingY.value}px;`;
-        result = `${codeString}${codeColor}
-    ${codeBgColor}
-    ${codePaddingX}
-    ${codeBorderRadius}
-${endCodeString}`;
-
-        inputCode.innerHTML = result;
-
-        button.style.padding = paddingX.value + "px " + paddingY.value + "px";
-
-    })
-
-    borderRadius.addEventListener("input", function() {
-        codeBorderRadius = `border-radius: ${borderRadius.value}px;`;
-        result = `${codeString}${codeColor}
-    ${codeBgColor}
-    ${codePaddingX}
-    ${codeBorderRadius}
-${endCodeString}`;
-
-        inputCode.innerHTML = result;
-
-        button.style.borderRadius = borderRadius.value + "px";
-
-    })
-    
-    document.querySelector("#copy").addEventListener("click", function() {
-        copyToClipboard(result);
-    })
-    
-}
-
-function copyToClipboard(text) {
-    window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
-  }
-  
+})();
